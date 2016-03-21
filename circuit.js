fd6e3c7669f5e430155f4ea9809197b8240ff159
Eric Fleming <ericfleming8@gmail.com>
2016-03-20 19:24:58 -0500

    outlined the class constructor

---
+++ circuit.js
@@ -0,0 +1,16 @@
+//exports a class circuit which allows you to use ohms laws and relaant methods.
+"use strict";
+
+class circuit{
+    constructor(circuitstring){
+        this.volts = Number(circuitstring.substring(circuitstring.indexOf('=')+1,circuitstring.length));
+        this.circuit = circuitstring.substring(circuitstring.indexOf('<<')+2,circuitstring.indexOf('>>')-1);
+    }
+
+}
+
+//Testing class methods
+var ohm = new circuit("<<200&300>>v=100");
+console.log(ohm);
+console.log(ohm.volts);
+console.log(ohm.circuit);
